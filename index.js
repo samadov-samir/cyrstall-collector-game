d57636c733e52f04e9257a435cba29d1d1b821bb
Samir <79440984+samadov-samir@users.noreply.github.com>
2023-02-19 14:09:05 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-var randomGenerator = Math.floor(Math.random() * (100 - 50) + 50);
 var cem = 0;
 var wins = 0
 var losses = 0
 document.querySelector('#xal').innerHTML = cem
 document.querySelector('#wins').innerHTML = wins
 document.querySelector('#losses').innerHTML = losses
-
+function random(){
+    var randomGenerator = Math.floor(Math.random() * (100 - 50) + 50);   
+    return randomGenerator
+}
 function resetValues() {
     //$("#xal").html(0);
     cem = 0
 }
-$('#comp').text(randomGenerator);
+$('#comp').text(random());
 $(".btn1").on("click", function () {
     var x = $(this).data("deyer");
     cem += parseInt(x);
@@ -18,10 +20,12 @@ $(".btn1").on("click", function () {
     if (cem === parseInt(document.querySelector("span").innerHTML)) {
         wins++;
         $("#wins").html(wins);
+        $('#comp').text(random());
         resetValues()
     } else if (cem > parseInt(document.querySelector("span").innerHTML)) {
         losses++;
         $("#losses").html(losses);
+        $('#comp').text(random());
         resetValues();
     } else {
         //alert("Umumi cem " + " " + cem);
