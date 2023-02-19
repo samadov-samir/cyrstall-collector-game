62e6b4b0aa85d829802a72c10fbef47fef2efba6
Samir <79440984+samadov-samir@users.noreply.github.com>
2023-02-19 14:21:51 +0400

    js file update

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
+const randomGenerator = ()=>{
+    return Math.floor(Math.random() * (100 - 50) + 50);
+}
 var cem = 0;
 var wins = 0
 var losses = 0
 document.querySelector('#xal').innerHTML = cem
 document.querySelector('#wins').innerHTML = wins
 document.querySelector('#losses').innerHTML = losses
-function random(){
-    var randomGenerator = Math.floor(Math.random() * (100 - 50) + 50);   
-    return randomGenerator
-}
 function resetValues() {
-    //$("#xal").html(0);
     cem = 0
 }
-$('#comp').text(random());
+$('#comp').text(randomGenerator());
 $(".btn1").on("click", function () {
     var x = $(this).data("deyer");
     cem += parseInt(x);
@@ -20,13 +18,15 @@ $(".btn1").on("click", function () {
     if (cem === parseInt(document.querySelector("span").innerHTML)) {
         wins++;
         $("#wins").html(wins);
-        $('#comp').text(random());
+        $('#comp').text(randomGenerator());
         resetValues()
+        document.querySelector('#xal').innerHTML = cem
     } else if (cem > parseInt(document.querySelector("span").innerHTML)) {
         losses++;
         $("#losses").html(losses);
-        $('#comp').text(random());
+        $('#comp').text(randomGenerator());
         resetValues();
+        document.querySelector('#xal').innerHTML = cem
     } else {
         //alert("Umumi cem " + " " + cem);
         document.querySelector('#xal').innerHTML = cem
